3e940c8401371e0127eb46ef88bddd7131a99118
YJH9254 <133298353+YJH9254@users.noreply.github.com>
2023-09-01 20:07:11 +0900

    Delete study0511 directory

--- study0511/Test2.java
+++
@@ -1,9 +0,0 @@
-package study0511;
-
-public class Test2 {
-
-		public static void main(String[] args) {
-			// 2023-11-11
-			System.out.println("내 생일: "+unixTimeTest3.doomday("2023-05-04"+"일 남았습니다."));
-		}
-}
