3f664cd77e4f071107def09137f5eafe79418148
YJH9254 <133298353+YJH9254@users.noreply.github.com>
2023-09-01 20:06:47 +0900

    Delete study1 directory

--- study1/Test2.java
+++
@@ -1,6 +0,0 @@
-package study1;
-
-public class Test2 {
-
-	String title = "java"; 
-	int eng = 100;
--- study1/test1004.java
+++
@@ -1,8 +0,0 @@
-package study1;
-
-import study1.test1004; 
-
-public class test1004 {
-	public String title = "java";
-	private int eng = 100;
-}
